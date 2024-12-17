0ca089a8fc9dca880404cc221bb4ee87c62c0040
Ismaël SACKO <sakoismael.is.1999@gmail.com>
2024-12-17 11:12:48 +0000

    dashboard

--- Dashboard/src/app/app-routing.module.ts
+++ Dashboard/src/app/app-routing.module.ts
@@ -1,27 +1,32 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import {DashboardComponent} from './dashboard/dashboard.component';
-import {BaseComponent} from './main/layout/base/base.component';
-
-const routes: Routes = [
-  {
-    path: 'admin',
-    component: BaseComponent,
-    children: [
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {
-        path: 'customers',
-        loadChildren: () => import('./entities/customer/customer.module').then(m => m.CustomerModule)
-      }
-    ]
-  }
-];
+import { NotfoundComponent } from './demo/components/notfound/notfound.component';
+import { AppLayoutComponent } from "./layout/app.layout.component";
+import { MydashboardComponent } from './demo/components/mydashboard/mydashboard.component';
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot([
+            {
+                path: '', component: AppLayoutComponent,
+                children: [
+                    { path: '', loadChildren: () => import('./demo/components/dashboard/dashboard.module').then(m => m.DashboardModule) },
+                    { path: 'uikit', loadChildren: () => import('./demo/components/uikit/uikit.module').then(m => m.UikitModule) },
+                    { path: 'utilities', loadChildren: () => import('./demo/components/utilities/utilities.module').then(m => m.UtilitiesModule) },
+                    { path: 'documentation', loadChildren: () => import('./demo/components/documentation/documentation.module').then(m => m.DocumentationModule) },
+                    { path: 'blocks', loadChildren: () => import('./demo/components/primeblocks/primeblocks.module').then(m => m.PrimeBlocksModule) },
+                    { path: 'pages', loadChildren: () => import('./demo/components/pages/pages.module').then(m => m.PagesModule) },
+                    // New Update Template
+                    { path: 'mydashboard', component: MydashboardComponent },
+                ],
+            },
+            { path: 'auth', loadChildren: () => import('./demo/components/auth/auth.module').then(m => m.AuthModule) },
+            { path: 'landing', loadChildren: () => import('./demo/components/landing/landing.module').then(m => m.LandingModule) },
+            { path: 'pages/notfound', component: NotfoundComponent },
+            { path: '**', redirectTo: 'pages/notfound' },
+        ], { scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled', onSameUrlNavigation: 'reload' })
+    ],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
